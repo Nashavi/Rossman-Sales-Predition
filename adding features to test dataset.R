9e086282d9b11e5d93032e0effbe4daebf91cff1
kbartsch <kyleb83@gmail.com>
2016-02-28 09:21:13 -0700

    just starting to add the features we will use for modeling to the test dataset.

---
+++ adding features to test dataset.R
@@ -0,0 +1,81 @@
+t <- read.csv("Datasets/test.csv")
+s <- read.csv("Datasets/Stores.csv")
+s<- s[,-1]
+
+t <- merge(t,s,by="Store",all.x = TRUE)
+rm(s)
+
+t$Date <- as.Date(t$Date,'%Y-%m-%d')
+t <- merge(t,calendar,by="Date",all.x = TRUE)
+
+#now just remove duplicated columns from merge
+t = t[,-which(names(t) %in% c("DayOfWeek"))] 
+
+t$max_t <- -1
+t$avg_t <- -1
+t$min_t <- -1
+t$max_wind <- -1
+t$precip <- -1
+t$cloudcover <- -1
+t$events <- ""
+
+airports <- unique(t[t$max_t==-1,"AirportCode"])
+
+#we'll process one airport at a time:
+for(a in 1:length(airports)){
+  ac <- airports[a]
+  
+  #generate a vecotor of stores for that airport code:
+  stores <- unique(t[t$AirportCode==ac,"Store"])
+  
+  #weather data package can only handle so many dates, so grab a vector of years to process:
+  years <- unique(t[t$Store %in% stores,"year"])
+  
+  #print(paste("Processing",year,"year for",ac,"airport"))
+  #now process one year at a time:
+  
+  for(k in 1:length(years)){
+    year <- years[k] 
+    
+    #what is the start date we should use?
+    begin <- min(t[t$Store %in% stores & t$year==year,"Date"])
+    #what is the end date to use?
+    end <- max(t[t$Store %in% stores & t$year==year,"Date"])
+   
+    #now grab weather for the airport using begin and end dates
+    dat <- as.data.frame(weatherData::getWeatherForDate(ac,begin,end,opt_all_columns=TRUE))
+    dat$Date <- as.Date(dat$Date)
+    
+    print(paste("Processing all dates for",ac," airport"))
+    
+    #loop through all weather dates and grab weather info for each one
+    for(j in 1:length(dat$Date)){ #now for each date in the weather data
+      dt <- dat$Date[j]
+      dt <- as.Date(dt,'%Y-%m-%d')
+      
+      #print(paste("Processing Date: ",dt)) #optional if you want to see detailed processing
+      #get weather features for the given date/airport: 
+      max_t <- dat$Max_TemperatureF[j]
+      avg_t <- dat$Mean_TemperatureF[j]
+      min_t <- dat$Min_TemperatureF[j]  
+      max_wind <- dat$Max_Wind_SpeedMPH[j] 
+      precip <- dat$PrecipitationIn[j] 
+      cloudcover <- dat$CloudCover[j] 
+      events <- dat$Events[j]
+      
+      #now assign those weather features to stores:
+      #match on storeid, airportcode, and date
+      t[t$Store %in% stores & t$AirportCode==ac & t$Date==dt,"max_t"] <- max_t
+      t[t$Store %in% stores & t$AirportCode==ac & t$Date==dt,"avg_t"] <- avg_t
+      t[t$Store %in% stores & t$AirportCode==ac & t$Date==dt,"min_t"] <- min_t
+      t[t$Store %in% stores & t$AirportCode==ac & t$Date==dt,"max_wind"] <- max_wind
+      t[t$Store %in% stores & t$AirportCode==ac & t$Date==dt,"precip"] <- precip
+      t[t$Store %in% stores & t$AirportCode==ac & t$Date==dt,"cloudcover"] <- cloudcover
+      t[t$Store %in% stores & t$AirportCode==ac & t$Date==dt,"events"] <- events
+    }
+    print(paste("Done processing data for",ac,", year",year))
+  }
+}
+print("Done")
+
+write.csv(t,"Datasets/test2.csv")
