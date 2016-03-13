5522d70130e4338a832b766cab880d2cacc8140d
kbartsch <kyleb83@gmail.com>
2016-03-13 14:16:52 -0600

    updated tempalte

--- Modeling/RandomForest Models/Test RF Model.r
+++ Modeling/RandomForest Models/Test RF Model.r
@@ -1,32 +1,53 @@
 
-
 # Load Default Setup 
 source("Modeling/Modeling Setup.R")
+unique(training$State)
+st <- "SN"
 
-d <- training
-rm(training)
 
 ##### training subsettting and prep ##### 
+d <- training
+rm(training)
 d <- d[,-1] #remove index
 d <- d[d$Open==1 & d$Sales!=0,] #subset open and >0 sales
 d <- d[,-2] #remove open 
 d <- d[,-3] #remove stores 
-
-unique(d$State)
-
-d <- d[d$State=="SN",] #choose a store and subset
+d <- d[d$State==st,] #choose a state and subset
 d <- d[,-2] #now remove state
-
 d$Sales <- log(d$Sales) #now change sales to log sales 
 
+ts <- testing
+rm(testing)
+ts <- ts[,-1] #remove index
+ts <- ts[ts$Open==1 & ts$Sales!=0,] #subset open and >0 sales
+ts <- ts[,-2] #remove open 
+ts <- ts[,-3] #remove stores 
+ts <- ts[ts$State==st,] #choose a state and subset
+ts <- ts[,-2] #now remove state
+ts$Sales <- log(ts$Sales) #now change sales to log sales 
+
+e <- eval
+rm(eval)
+e <- e[,-1] #remove index
+e <- e[e$Open==1 & e$Sales!=0,] #subset open and >0 sales
+e <- e[,-2] #remove open 
+e <- e[,-3] #remove stores 
+e <- e[e$State==st,] #choose a state and subset
+e <- e[,-2] #now remove state
+e$Sales <- log(e$Sales) #now change sales to log sales 
 
 
-##### model hyperparamter eval ################## 
+
+################### MODEL TUNING ################## 
+
+p = dim(d)[2]-1 #how many variables are available?
+p <- round(p/3) #mytry should be ~ p/3
+mtry.vec <- c(p-30,p-20,p-10,p,p+10,p+20,p+30) ### try p/3 bracketed up and down -30 tp + 30
 
 fitControl = trainControl(method = 'cv'
-                          , number=3
+                          , number=5
                           ,summaryFunction=defaultSummary)
-Grid =  expand.grid(mtry=10)#c(90,100,110,120,130,140,150))
+Grid =  expand.grid(mtry=mtry.vec)
 rfFit = train(Sales ~ .
               ,data = d
               ,method='rf'
@@ -35,22 +56,14 @@ rfFit = train(Sales ~ .
               ,tuneGrid=Grid
               ,verbose=TRUE)
 
+save(rfFit,file="Modeling/RandomForest Models/SN_RFModel.RData",compress = TRUE)
 
+plot(rfFit)
+summary(rfFit)[1:10,]
 
+test.preds <- predict(rfFit,newdata = ts)
+eval.preds <- predict(rfFit,newdata = e)
 
+sqrt(mean((ts$Sales-test.preds)^2))
+sqrt(mean((e$Sales-eval.preds)^2))
 
-
-load("Datasets/NewFinalTest.RData")
-
-
-
-t$Sales <- ifelse(t$Open==0,0,test.preds <- predict(gbmFit,newdata = t))
-
-X <- t$X
-Open <- t$Open
-State <- t$State
-Sales <- t$Sales
-
-new <- cbind(Sales,State)
-new <- cbind(Open,new)
-new <- cbind(X,new)
