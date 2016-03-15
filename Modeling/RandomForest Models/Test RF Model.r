b6e77e8d1b51051326cf89c1b81bac5e42b4c83c
kbartsch <kyleb83@gmail.com>
2016-03-14 19:19:45 -0600

    added some logic to optimize memory usage

--- Modeling/RandomForest Models/Test RF Model.r
+++ Modeling/RandomForest Models/Test RF Model.r
@@ -5,6 +5,8 @@ source("Modeling/Modeling Setup.R")
 unique(training$State)
 st <- "NWRP"
 
+rm(testing) #remove to optimize memory 
+rm(eval) #remove to optimize memory 
 
 ##### training subsettting and prep ##### 
 d <- training
@@ -16,6 +18,36 @@ d <- d[,-3] #remove stores
 d <- d[d$State==st,] #choose a state and subset
 d <- d[,-2] #now remove state
 d$Sales <- log(d$Sales) #now change sales to log sales 
+
+################### MODEL TUNING ################## 
+
+p = dim(d)[2]-1 #how many variables are available?
+p <- round(p/3) #mytry should be ~ p/3
+mtry.vec <- c(110,120,130) ### try p/3 bracketed up and down -30 tp + 30
+
+fitControl = trainControl(method = 'cv'
+                          , number=3
+                          ,verboseIter = T
+                          ,summaryFunction=defaultSummary)
+Grid =  expand.grid(mtry=mtry.vec)
+rfFit = train(Sales ~ .
+              ,data = d
+              ,method='rf'
+              ,trControl=fitControl
+              ,metric="RMSE"
+              ,tuneGrid=Grid
+              ,verbose=TRUE)
+
+save(rfFit,file="Modeling/RandomForest Models/NWRP_RFModel.RData",compress = TRUE)
+
+plot(rfFit)
+summary(rfFit)[1:10,]
+
+#### NOW LOAD TESTING AND EVAL TO SEE HOW GOOD RMSE IS #####
+
+source("Modeling/Modeling Setup.R")
+rm(d) #memory
+rm(training) #memory 
 
 ts <- testing
 rm(testing)
@@ -39,29 +71,6 @@ e$Sales <- log(e$Sales) #now change sales to log sales
 
 
 
-################### MODEL TUNING ################## 
-
-p = dim(d)[2]-1 #how many variables are available?
-p <- round(p/3) #mytry should be ~ p/3
-mtry.vec <- c(110,120,130) ### try p/3 bracketed up and down -30 tp + 30
-
-fitControl = trainControl(method = 'cv'
-                          , number=3
-                          ,verboseIter = T
-                          ,summaryFunction=defaultSummary)
-Grid =  expand.grid(mtry=mtry.vec)
-rfFit = train(Sales ~ .
-              ,data = d
-              ,method='rf'
-              ,trControl=fitControl
-              ,metric="RMSE"
-              ,tuneGrid=Grid
-              ,verbose=TRUE)
-
-save(rfFit,file="Modeling/RandomForest Models/HE_RFModel.RData",compress = TRUE)
-
-plot(rfFit)
-summary(rfFit)[1:10,]
 
 test.preds <- predict(rfFit,newdata = ts)
 eval.preds <- predict(rfFit,newdata = e)
