c9092dae06a16b713657088793493103bfc32519
kbartsch <kyleb83@gmail.com>
2016-04-07 20:48:47 -0600

    update

--- Feature Engineering/Adding Sales Lags.R
+++ Feature Engineering/Adding Sales Lags.R
@@ -19,3 +19,14 @@ dlagged <- slide(d, Var = "Sales", GroupVar = "Store", NewVar= "SixWeekSalesLag"
 training <- dlagged
 
 save(training,file="Datasets/TreeTraining.RData",compress = TRUE)
+
+
+##### For TEst Dataset
+
+# lag.stores <- unique(d[is.na(d$SixWeekSalesLag),"Store"])
+# 
+# for(i in 1:length(lag.stores)){
+#   avg.sales <- mean(d[d$Store==i & d$year==2013 & d$month==1,"Sales"])
+#   d[d$Store==i & is.na(d$SixWeekSalesLag)==TRUE,"SixWeekSalesLag"] <- avg.sales
+#   print(paste(i,avg.sales))
+# }
